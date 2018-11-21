21042d7ea5cf5b143410f4f13ba0427cfe1f52f6
sfaruk3131 <sfaruk3131@bths.edu>
2018-11-21 10:13:00 -0500

    2

--- src/inPlaceSorts.java
+++ src/inPlaceSorts.java
@@ -1,7 +1,29 @@
 public class inPlaceSorts {
-    public static void insertionSort()
+    public static void insertionSort(int[] list1)
     {
 
     }
-    public static void selection
+    public static void selectionSort(double[] list1)
+    {
+        int len = list1.length;
+
+        for(int i = 0; i < len - 1; i++)
+        {
+            int minidx = i;
+            for(int j = i +1; j < len; j++)
+            {
+                if(list1[j] < list1[minidx])
+                {
+                    minidx = j;
+                }
+                int temp = (int)list1[minidx];
+                list1[minidx] = list1[i];
+                list1[i] = temp;
+            }
+        }
+    }
+    public static void bubbleSort(String[] list1)
+    {
+
+    }
 }
